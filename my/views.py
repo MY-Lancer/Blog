b994e35fad2eb3c429c9c97c029115728b555b2b
MY-Lancer <2823797582@qq.com>
2020-05-29 21:01:13 +0800

    整理前端文件

--- my/views.py
+++ my/views.py
@@ -1,3 +1,24 @@
+# ---------------------------
+__author__ = 'my'
+__date__ = '2020/5/29 20:31'
+# ---------------------------
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views import generic
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Article, BigCategory, Category, Tag
+
+# Create your views here.
+class IndexView(generic.ListView):
+    """
+        首页视图,继承自ListVIew，用于展示从数据库中获取的文章列表
+    """
+    # 获取数据库中的文章列表
+    model = Article
+    # template_name属性用于指定使用哪个模板进行渲染
+    template_name = 'index.html'
+    # context_object_name属性用于给上下文变量取名（在模板中使用该名字）
+    context_object_name = 'articles'
